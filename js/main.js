3f2690e1537dfafb3559be245dca5f4ed96e72c0
Delink-D <delinkdesigns@gmail.com>
2017-10-19 20:18:18 +0300

    function to get people after click of button, try it

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+  var btnView = document.getElementById('btn-view');
+
+  btnView.addEventListener('click', function() {
+    alert('Hahahaha gotcha!!!');
+  }, false);
+
+});
